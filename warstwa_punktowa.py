96ab417c897f2f93e93c88e0acd3d06ffaf88107
ainglot <ainglot@gmail.com>
2025-11-18 09:00:57 +0100

    Repozytorium Most Wiedzy

--- warstwa_punktowa.py
+++ warstwa_punktowa.py
@@ -5,7 +5,11 @@ import arcpy
 # ===================================================================
 arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\PPA_Gr3.gdb"
 warstwa_drzew = "GDA2020_OT_OIPR_P_copy"
+# https://mostwiedzy.pl/pl/open-research-data/3d-point-cloud-as-a-representation-of-silo-tank,615070441641526-0
 
+# ===================================================================
+# ZDEFINIOWANE FUNKCJE DLA WARSTWY PUNKTOWEJ
+# ===================================================================
 def odczytywanie_wspolrzednych(warstwa):
     with arcpy.da.SearchCursor(warstwa, ["SHAPE@X", "SHAPE@Y"]) as cursor:
         for row in cursor:
@@ -19,6 +23,19 @@ def aktualizacja_wspolrzednych(warstwa):
             row[1] += 100
             print(row)
             cursor.updateRow(row) 
+
+
+def wstawianie_wspolrzednych(warstwa):
+    with arcpy.da.UpdateCursor(warstwa, ["SHAPE@X", "SHAPE@Y"]) as cursor:
+        for row in cursor:
+            row[0] += 100
+            row[1] += 100
+            print(row)
+            cursor.updateRow(row) 
+
+def nowa_warstwa_punktowa(nazwa_warstwy, uklad_wsp):
+    arcpy.management.CreateFeatureclass(arcpy.env.workspace, nazwa_warstwy, "POINT", "", "DISABLED", "DISABLED", uklad_wsp)
+
 
 # odczytywanie_wspolrzednych(warstwa_drzew)
 aktualizacja_wspolrzednych(warstwa_drzew)
