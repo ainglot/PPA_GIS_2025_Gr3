ce883bd992ea1d7e4a32507295d4e04f5b1acdec
ainglot <ainglot@gmail.com>
2025-11-18 08:51:28 +0100

    Modyfikacja współrzędnych warstwy punktowej

--- warstwa_punktowa.py
+++ warstwa_punktowa.py
@@ -4,11 +4,23 @@ import arcpy
 # KONFIGURACJA ŚRODOWISKA
 # ===================================================================
 arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\PPA_Gr3.gdb"
-warstwa_drzew = "GDA2020_OT_OIPR_P"
+warstwa_drzew = "GDA2020_OT_OIPR_P_copy"
 
-cursor = arcpy.da.SearchCursor(warstwa_drzew, ["SHAPE@X", "SHAPE@Y"])
-for row in cursor:
-    print(row)
+def odczytywanie_wspolrzednych(warstwa):
+    with arcpy.da.SearchCursor(warstwa, ["SHAPE@X", "SHAPE@Y"]) as cursor:
+        for row in cursor:
+            print(row)
 
+
+def aktualizacja_wspolrzednych(warstwa):
+    with arcpy.da.UpdateCursor(warstwa, ["SHAPE@X", "SHAPE@Y"]) as cursor:
+        for row in cursor:
+            row[0] += 100
+            row[1] += 100
+            print(row)
+            cursor.updateRow(row) 
+
+# odczytywanie_wspolrzednych(warstwa_drzew)
+aktualizacja_wspolrzednych(warstwa_drzew)
 
 print("KONIEC")
