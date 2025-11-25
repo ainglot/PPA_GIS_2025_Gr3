4e9cc94f0bc961eea528ff7fe263bf3ceb4d1620
ainglot <ainglot@gmail.com>
2025-11-25 08:42:06 +0100

    Odczytywanie współrzędnych wierzchołków linii

--- OdczytywanieZapisPoly.py
+++ OdczytywanieZapisPoly.py
@@ -13,23 +13,27 @@ warstwa_poly_in = "GDA2014_OT_SWRS_L"
 def odczytywanie_wspolrzednych(warstwa):
     i = 0
     ListaWsp = []
-    with arcpy.da.SearchCursor(warstwa, ["SHAPE@X", "SHAPE@Y"]) as cursor:
+    with arcpy.da.SearchCursor(warstwa, ["SHAPE@"]) as cursor:
         for row in cursor:
             i += 1
             print(i, row)
-            ListaWsp.append(row)
+            for parth in row[0]:
+                print(parth)
+                for pnt in parth:
+                    print("  ", pnt.X, pnt.Y)
+            # ListaWsp.append(row)
     return ListaWsp
 
-def nowa_warstwa_punktowa(nazwa_warstwy, uklad_wsp, list_coor):
-    # list_coor = odczytywanie_pliku_txt("data.txt")
-    arcpy.management.CreateFeatureclass(arcpy.env.workspace, nazwa_warstwy, "POINT", "", "DISABLED", "DISABLED", uklad_wsp)
-    with arcpy.da.InsertCursor(nazwa_warstwy, ["SHAPE@X", "SHAPE@Y"]) as cursor:
-        for coor in list_coor:
-            X = coor[0]
-            Y = coor[1]
-            cursor.insertRow([X, Y])
+# def nowa_warstwa_punktowa(nazwa_warstwy, uklad_wsp, list_coor):
+#     # list_coor = odczytywanie_pliku_txt("data.txt")
+#     arcpy.management.CreateFeatureclass(arcpy.env.workspace, nazwa_warstwy, "POINT", "", "DISABLED", "DISABLED", uklad_wsp)
+#     with arcpy.da.InsertCursor(nazwa_warstwy, ["SHAPE@X", "SHAPE@Y"]) as cursor:
+#         for coor in list_coor:
+#             X = coor[0]
+#             Y = coor[1]
+#             cursor.insertRow([X, Y])
 
 ListaWSP = odczytywanie_wspolrzednych(warstwa_poly_in)
-print(ListaWSP)
-nowa_warstwa_punktowa("CentroidyRWSR", warstwa_poly_in, ListaWSP)
+# print(ListaWSP)
+# nowa_warstwa_punktowa("CentroidyRWSR", warstwa_poly_in, ListaWSP)
 print("KONIEC")
