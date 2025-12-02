b09bc3676a72bc1ec47ebaa1d509b40f1c55a544
ainglot <ainglot@gmail.com>
2025-12-02 09:54:34 +0100

    Praca z objektem geometry

--- objet_geometry.py
+++ objet_geometry.py
@@ -8,7 +8,46 @@ warstwa_poligonowa = "Budynki"
 
 geometry = arcpy.management.CopyFeatures(warstwa_poligonowa, arcpy.Geometry())
 
-for geo in geometry:
-    print(geo)
+def odczytywanie_wspolrzednych_poligonu(warstwa):
+    i = 0
+    ListaOb = []
+    Centr = []
+    with arcpy.da.SearchCursor(warstwa, ["SHAPE@", "SHAPE@XY"]) as cursor:
+        for row in cursor:
+            print(row)
+            i += 1
+            ListaPart = []
+            Centr.append(row[1])
+            for parth in row[0]:
+                ListaWsp = []
+                for pnt in parth:
+                    # print(pnt)
+                    if pnt:
+                        ListaWsp.append([pnt.X, pnt.Y])
+                    else:
+                        ListaPart.append(ListaWsp)
+                        ListaWsp = []
+                ListaPart.append(ListaWsp)
+            ListaOb.append(ListaPart)
+    return ListaOb, Centr
+
+Otoczki = []
+i = 0
+
+for geo1 in geometry:
+    # j = 0
+    # for geo2 in geometry:
+    #     if i < j:
+    #         if geo1.touches(geo2):
+    #             print(i, j, geo1.touches(geo2))
+    #         print(i, j, geo1.distanceTo(geo2))
+    #     j += 1
+    # i += 1
+    print(geo1.getArea("GEODESIC", "SquareMeters"), geo1.area)# XMin, YMin, XMax, YMax
+    # Otoczki.append(geo.convexHull().buffer(10).boundary())
+
+
+# arcpy.management.CopyFeatures(Otoczki, "Budynki_11")
+# odczytywanie_wspolrzednych_poligonu(warstwa_poligonowa)
 
 print("KONIEC")
