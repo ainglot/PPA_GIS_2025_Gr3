c2eeefbe24e3de6349d22549c581b5aaac19c877
ainglot <ainglot@gmail.com>
2025-12-16 09:43:08 +0100

    Poprawki.

--- linia3d.py
+++ linia3d.py
@@ -34,12 +34,13 @@ def punkt_na_rastrze(punkt, zakres_rastra):
 def nowa_warstwa_punktowa(nazwa_warstwy, uklad_wsp, list_coor):
     # list_coor = odczytywanie_pliku_txt("data.txt")
     arcpy.management.CreateFeatureclass(arcpy.env.workspace, nazwa_warstwy, "POINT", "", "DISABLED", "ENABLED", uklad_wsp)
-    with arcpy.da.InsertCursor(nazwa_warstwy, ["SHAPE@X", "SHAPE@Y", "SHAPE@Z"]) as cursor:
+    arcpy.management.AddField(nazwa_warstwy, "coorZ", "DOUBLE")
+    with arcpy.da.InsertCursor(nazwa_warstwy, ["SHAPE@X", "SHAPE@Y", "SHAPE@Z", "coorZ"]) as cursor:
         for coor in list_coor:
             X = coor[0]
             Y = coor[1]
             Z = coor[2]
-            cursor.insertRow([X, Y, Z])
+            cursor.insertRow([X, Y, Z, Z])
 
 
 WspLini = odczytywanie_wspolrzednych(warstwa_linii)
@@ -79,6 +80,7 @@ for linia in WspLini:
                 print(dx, dy, R_array[row, col])
                 ListaPKT.append([PKT[0], PKT[1], R_array[row, col]])
 
+arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\Geobaza ZTM\ZTM197.gdb"
 nowa_warstwa_punktowa("Punkty3D_ZTM", warstwa_linii, ListaPKT)
 
 print("KONIEC")
