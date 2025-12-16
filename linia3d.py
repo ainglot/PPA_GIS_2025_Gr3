08e55a94775071db7d06027762e933229606e068
ainglot <ainglot@gmail.com>
2025-12-16 08:55:10 +0100

    Zakres współrzędnych rastra.

--- linia3d.py
+++ linia3d.py
@@ -1,10 +1,10 @@
 import arcpy
 
 # ===================================================================
-# KONFIGURACJA ŚRODOWISKA
+# KONFIGURACJA ŚRODOWISKA DLA WEKTORÓW
 # ===================================================================
 arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\Geobaza ZTM\ZTM197.gdb"
-warstwa_linii = "ZTM_195"
+warstwa_linii = "ZTM_195_PL92"
 
 # ===================================================================
 # ZDEFINIOWANE FUNKCJE DLA WARSTWY LINIOWEJ LUB POLIGONOWEJ
@@ -26,3 +26,20 @@ def odczytywanie_wspolrzednych(warstwa):
 
 WspLini = odczytywanie_wspolrzednych(warstwa_linii)
 print(WspLini)
+
+# ===================================================================
+# KONFIGURACJA ŚRODOWISKA DLA RASTRÓW
+# ===================================================================
+arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\NMT pod ZTM\ZTM197_NMT_TIF"
+
+ListaExtentR = []
+rasters = arcpy.ListRasters("*", "TIF")
+for raster in rasters:
+    print(raster)
+    R = arcpy.Raster(raster)
+    print("Extent:", R.extent)
+    ListaExtentR.append([raster, [R.extent.XMin, R.extent.YMin, R.extent.XMax, R.extent.YMax]])
+
+
+
+print(ListaExtentR)
